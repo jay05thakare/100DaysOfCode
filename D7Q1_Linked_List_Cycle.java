3c1cc95e909f38c729d08379853e0d371d9eb93b
Jay <jay05thakare@gmail.com>
2022-07-25 19:12:21 +0530

    Day 6

---
+++ D7Q1_Linked_List_Cycle.java
@@ -0,0 +1,43 @@
+public class D7Q1_Linked_List_Cycle {
+
+  static Node head;
+
+  static class Node{
+    int data;
+    Node next;
+
+    Node(int d){
+      data = d;
+      next = null;
+    }
+  }
+
+  void hasCycle(){
+    Node fast = head;
+        Node slow = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow){
+              System.out.println("list has cycle");
+                return;
+            }
+        }
+        System.out.println("list doesn't has cycle");
+        return;
+  }
+  public static void main(String[] args) {
+    D7Q1_Linked_List_Cycle list = new D7Q1_Linked_List_Cycle();
+
+    list.head = new Node(85);
+		list.head.next = new Node(15);
+		list.head.next.next = new Node(4);
+
+    // create loop for testing purpose
+		list.head.next.next.next = list.head.next;
+
+    list.hasCycle() ;
+  }
+}
