b6ed0b08cdc5f00aae265e193db54cc6aff921cb
Jay <jay05thakare@gmail.com>
2022-07-26 15:17:47 +0530

    Day 8

---
+++ D8Q4_Implement_Two_Stack_In_Array.java
@@ -0,0 +1,59 @@
+// here's my code
+// https://practice.geeksforgeeks.org/viewSol.php?subId=c997317329a64a1f533181bc3049b3c0&pid=700237&user=jay05thakare
+
+
+
+
+/* Structure of the class is
+class TwoStack
+{
+
+	int size;
+	int top1,top2;
+	int arr[] = new int[100];
+
+	TwoStack()
+	{
+		size = 100;
+		top1 = -1;
+		top2 = size;
+	}
+}*/
+
+/*class Stacks
+{
+    //Function to push an integer into the stack1.
+    void push1(int x, TwoStack sq)
+    {
+        sq.top1 += 1;
+        sq.arr[sq.top1] = x;
+    }
+
+    //Function to push an integer into the stack2.
+    void push2(int x, TwoStack sq)
+    {
+        sq.top2 -= 1;
+        sq.arr[sq.top2] = x;
+    }
+
+    //Function to remove an element from top of the stack1.
+    int pop1(TwoStack sq)
+    {
+        if(sq.top1 == -1){
+            return -1;
+        }else{
+            return sq.arr[sq.top1--];
+        }
+    }
+
+    //Function to remove an element from top of the stack2.
+    int pop2(TwoStack sq)
+    {
+        if(sq.top2 == sq.size){
+            return -1;
+        }else{
+            return sq.arr[sq.top2++];
+        }
+    }
+}
+*/
