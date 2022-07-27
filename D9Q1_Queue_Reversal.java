3b949f1f2c436d691351512894ace75d80049788
Jay <jay05thakare@gmail.com>
2022-07-27 16:42:32 +0530

    Day 9

---
+++ D9Q1_Queue_Reversal.java
@@ -0,0 +1,26 @@
+// find my code here:
+// https://practice.geeksforgeeks.org/viewSol.php?subId=0b126c5722e2fd9b867a974aac43752d&pid=700646&user=jay05thakare
+
+
+
+/*
+class GfG{
+  //Function to reverse the queue.
+  Stack<Integer> stack = new Stack<>();
+  
+  public Queue<Integer> rev(Queue<Integer> q){
+      //add code here.
+      while(!q.isEmpty()){
+          int top = q.remove();
+          stack.push(top);
+      }
+      
+      while(!stack.isEmpty()){
+          int top = stack.pop();
+          q.add(top);
+      }
+      
+      return q;
+  }
+}
+*/
