b1fd52c5930952b1428472735bd9fcc2f86fc411
Jay <jay05thakare@gmail.com>
2022-08-17 14:16:25 +0530

    Day 21

---
+++ D21Q1_Build_BST.java
@@ -0,0 +1,52 @@
+public class D21Q1_Build_BST {
+
+  static class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  public static Node buildBST(Node root, int val) {
+    if(root == null){
+      root = new Node(val);
+      return root;
+    }
+
+    if(root.data > val){
+      root.left = buildBST(root.left, val);
+    }else{
+      root.right = buildBST(root.right, val);
+    }
+
+    return root;
+  }
+
+  public static void inorder(Node root) {
+    if(root == null){
+      return;
+    }
+
+    inorder(root.left);
+    System.out.print(root.data);
+    inorder(root.right);
+  }
+  
+  public static void main(String[] args) {
+    int values[] = {5,1,3,4,2,7};
+    Node root = null;
+
+    for(int i = 0; i<values.length; i++){
+      root = buildBST(root, values[i]);
+    }
+
+    // printing inorder traversal of BST
+    inorder(root);
+    System.out.println();
+  }
+}
